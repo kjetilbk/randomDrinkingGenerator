82a6c5bba6c81e5c057770521c0fa3ddc8bf7c0c
Kjetil Bugge Kristoffersen <kjetil.kristoffersen@autodesk.com>
2022-04-24 18:24:36 +0200

    chore: refactor the code

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
 const poissonProcess = require("poisson-process");
 const exec = require("await-exec");
 
-const meanMs = 10000;
+//Every 25 minutes on avg.
+const meanMs = 25 * 60 * 1000;
 
-const beforeSaturday13List = [
-  "Sjur",
-  "Kjetil",
-  "Nora",
-  "Mirza",
-  "Solveig",
-  "Sunniva",
-  "Øystein",
-];
-
-const afterSaturday13List = [
+const names = [
   "Birk",
   "Sjur",
   "Kjetil",
@@ -23,53 +14,46 @@ const afterSaturday13List = [
   "Sunniva",
   "Øystein",
 ];
-const slurkRange = [1, 2, 3, 4, 5];
 
-const nameShotsBeforeSaturday13 = beforeSaturday13List.flatMap((name) =>
-  slurkRange.map((slurk) => [name, slurk])
-);
-
-const nameShotsAfterSaturday13 = afterSaturday13List.flatMap((name) =>
-  slurkRange.map((slurk) => [name, slurk])
-);
+const sipRange = [1, 2, 3, 4, 5];
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-const p = poissonProcess.create(meanMs, async function message() {
-  const now = new Date();
-  const saturday13 = new Date(2022, 3, 23, 13, 39);
-  const index =
-    Math.floor(Math.random() * nameShotsBeforeSaturday13.length) + 1;
-  if (now.getTime() < saturday13.getTime()) {
-    console.log("before");
-    await exec("afplay /Users/kjetilbk/Downloads/alert.wav");
+async function communicateSips(name, sipCount) {
+  console.log(`Giving ${sipCount} sips to ${name}`);
+  await exec("afplay ./alert.wav");
+  await exec(`say Congratulations to ${name}...`);
+  const random = Math.random();
+  if (random > 0.5 && random < 0.7) {
+    await exec(`say Happy birthday Solveig, you are a fucker`);
+  }
+  if (random > 0.9) {
     await exec(
-      `say Congratulations to ${nameShotsBeforeSaturday13[index][0]}...`
+      `say These ${sipCount} sips goes out to everyone. Lift your glasses.`
     );
-    await sleep(2000);
-    if (Math.random() > 0.5) {
-      await exec(`say Happy birthday Solveig, you are a star!`);
-    }
-    await exec(
-      `say You are hereby given the solid amount of... ${nameShotsBeforeSaturday13[index][1]} sips`
-    );
-    await exec(`say Best of luck. Yours truly, Sjur and Kjetil`);
+    await sleep(500);
+    await exec(`say Drink like its your last drink`);
+    await sleep(200);
+    await exec("say You little wankers");
+    await exec("say Cheers");
   } else {
-    console.log("after");
-    await exec("afplay /Users/kjetilbk/Downloads/alert.wav");
-    await exec(
-      `say Congratulations to ${nameShotsAfterSaturday13[index][0]}...`
-    );
-    const random = Math.random();
-    if (random > 0.5) {
-      await exec(`say Happy birthday Solveig, you are a fucker`);
-    }
     await sleep(2000);
     await exec(
-      `say You are hereby given the solid amount of... ${nameShotsAfterSaturday13[index][1]} sips`
+      `say You are hereby given the solid amount of... ${sipCount} sips`
     );
     await exec(`say Best of luck. Yours truly, Sjur and Kjetil`);
   }
-});
+}
 
-p.start();
+async function generateRandomDrink() {
+  const name = names[Math.floor(Math.random() * names.length) + 1];
+  const sipCount = sipRange[Math.floor(Math.random() * sipRange.length) + 1];
+  await communicateSips(name, sipCount);
+}
+
+async function randomDrinkingGenerator() {
+  await generateRandomDrink();
+  setTimeout(randomDrinkingGenerator, poissonProcess.sample(meanMs));
+}
+
+setTimeout(randomDrinkingGenerator, poissonProcess.sample(meanMs));
